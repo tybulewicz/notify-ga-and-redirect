dab951188ee20e7b1b8d3679f72a4de0d6c90b00
Tomasz Tybulewicz <tomasz@tybulewicz.com>
2015-05-22 15:17:03 +0200

    referrer spamers won't be sent to google analytics

--- public_html/index.php
+++ public_html/index.php
@@ -5,21 +5,31 @@ require '../config.php';
 use Krizon\Google\Analytics\MeasurementProtocol\MeasurementProtocolClient;
 use Rhumsaa\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\Request;
+use Tybulewicz\ReferrerSpamDetector\Detector;
 
 $request = Request::createFromGlobals();
 
-$client = MeasurementProtocolClient::factory(array(
-    'ssl' => false // Enable/Disable SSL, default false
-));
-$client->pageview(array(
-    'tid' => $config['tracking-id'],
-    'dh' => $config['hostname'],
-    'dp' => $config['page'],
-    'dt' => $config['title'],
-    'cid' => Uuid::uuid4(),
-    'ua' => $request->headers->get('User-Agent'),
-    'uip' => $request->getClientIp(),
-));
+$isSpam = false;
+if ($request->server->has('HTTP_REFERER')) {
+    $referrer = $request->server->get('HTTP_REFERER');
+    $detector = new Detector();
+    $isSpam = $detector->isSpam($referrer);
+}
+
+if (!$isSpam) {
+    $client = MeasurementProtocolClient::factory(array(
+        'ssl' => false // Enable/Disable SSL, default false
+    ));
+    $client->pageview(array(
+        'tid' => $config['tracking-id'],
+        'dh' => $config['hostname'],
+        'dp' => $config['page'],
+        'dt' => $config['title'],
+        'cid' => Uuid::uuid4(),
+        'ua' => $request->headers->get('User-Agent'),
+        'uip' => $request->getClientIp(),
+    ));
+}
 
 header("Location: {$config['link']}", true, 302);
 exit();
